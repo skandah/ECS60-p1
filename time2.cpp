cab5cfda570989b65582a2ebd28d2aca1a353a91
Paari Kandappan <paari.kandappan@gmail.com>
2012-04-06 22:13:31 -0700

    Fixed issue with ifstream parameter for ADT functions

--- time2.cpp
+++ time2.cpp
@@ -1,58 +1,103 @@
 #include <iostream>
 #include <fstream>
-#include <stdio.h>
-
 #include "CPUTimer.h"
-//clude "LinkedList.cpp"
-//clude "StackAr.cpp"
-//#include "dsexceptions.h"
-//clude "CursorList2.cpp"
 #include "LinkedList.h"
 #include "StackAr.h"
 #include "CursorList2.h"
-//clude "QueueAr.cpp"
-//clude "StackLi.cpp"
-//clude "vector.cpp"
 #include "QueueAr.h"
 #include "StackLi.h"
-//#include "vector.h"
 #include "SkipList.h"
-//#include "SkipList.cpp"
 using namespace std; 
 vector<CursorNode <int> > cursorSpace; 
 
-
-void RunList(FILE *file)
+void RunList(ifstream &file)
 {
-	char command[1000];
-	int i = -1; 
-	//char temp;  
-	do
+	string command, action, data;
+
+	while(!file.eof())
 	{
-		i++; 
-		fscanf(file, "%c", &command[i]); 
-		cout << command[i] << endl; 
+		file >> command;
+		action = command.substr(0,1);
+		data = command.substr(1);
 
-	}while(command[i] != EOF);
-
+		cout << action << endl;
+		cout << data << endl;
+	}
 }
 
+void RunCursorList(ifstream &file)
+{
+	string command, action, data;
 
+	while(!file.eof())
+	{
+		file >> command;
+		action = command.substr(0,1);
+		data = command.substr(1);
 
-void RunCursorList(FILE *file){
-	cout << " ";
-}  
-void RunStackAr(FILE *file){
-	cout << " ";
-}  
-void RunStackLi(FILE *file){
-	cout << " ";
-}  
-void RunQueueAr(FILE *file){
-	cout << " ";
-} 
-void RunSkipList(FILE *file){
-	cout << " ";
+		cout << action << endl;
+		cout << data << endl;
+	}
+}
+
+void RunStackAr(ifstream &file)
+{
+	string command, action, data;
+
+	while(!file.eof())
+	{
+		file >> command;
+		action = command.substr(0,1);
+		data = command.substr(1);
+
+		cout << action << endl;
+		cout << data << endl;
+	}
+}
+
+void RunStackLi(ifstream &file)
+{
+	string command, action, data;
+
+	while(!file.eof())
+	{
+		file >> command;
+		action = command.substr(0,1);
+		data = command.substr(1);
+
+		cout << action << endl;
+		cout << data << endl;
+	}
+}
+
+void RunQueueAr(ifstream &file)
+{
+	string command, action, data;
+
+	while(!file.eof())
+	{
+		file >> command;
+		action = command.substr(0,1);
+		data = command.substr(1);
+
+		cout << action << endl;
+		cout << data << endl;
+	}
+}
+
+void RunSkipList(ifstream &file)
+{
+	string command, action, data;
+
+	while(!file.eof())
+	{
+		file >> command;
+		action = command.substr(0,1);
+		data = command.substr(1);
+
+		cout << action << endl;
+		cout << data << endl;
+	}
 }
 
 int getChoice()
@@ -78,39 +123,30 @@ int main()
 {
 	int choice = 0; 
 	CPUTimer ct; 
-	FILE *file; 
 	string filename; 
 
+	//Get file with list of instructions
 	cout << "Filename >> "; 
 	cin >> filename;
-
-	file = fopen(filename.c_str(), "r"); 
-
-
-	//ifstream file();
+	ifstream file(filename.c_str(), ifstream::in);
 	
-
+	//Main action
 	do
-{
+	{
+		choice = getChoice();
+		ct.reset();
 
-     choice = getChoice();
-
-     ct.reset();
-
-     switch (choice)
-
-     {
-
-       case 1: RunList(file); break;
-       case 2: RunCursorList(file); break;
-       case 3: RunStackAr(file); break;
-       case 4: RunStackLi(file); break;
-       case 5: RunQueueAr(file); break;
-       case 6: RunSkipList(file); break;
-     }
-     cout << "CPU time: " << ct.cur_CPUTime() << endl;
+ 		switch (choice)
+   		{
+			case 1: RunList(file); break;
+	       	case 2: RunCursorList(file); break;
+	       	case 3: RunStackAr(file); break;
+	       	case 4: RunStackLi(file); break;
+	       	case 5: RunQueueAr(file); break;
+	       	case 6: RunSkipList(file); break;
+     	}
+     	cout << "CPU time: " << ct.cur_CPUTime() << endl;
 	} while(choice > 0);
-	fclose(file); 
-	return 0; 
 
+	return 0; 
 }
