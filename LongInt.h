a5debb7d1507c1481cbf820f31906e9d09af5f8f
Paari Kandappan <paari.kandappan@gmail.com>
2012-04-07 17:36:04 -0700

    Created LongInt source and header files

---
+++ LongInt.h
@@ -0,0 +1,12 @@
+#ifndef _STACKAR_H
+#define _STACKAR_H
+
+// LongInt class
+//
+// CONSTRUCTION:
+//
+// ******************PUBLIC OPERATIONS*********************
+//
+// ******************ERRORS********************************
+
+#endif
